f71e87da8911b2e4f332da82600b88229db42ec4
贝子 <2606754686@qq.com>
2021-01-14 11:05:32 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <!--
+      分为两个部分：
+      一：路由
+      二：导航
+  -->
+    <div class="App">
+        <router-view></router-view>
+        <FooterGuide v-show="$route.meta.showFooter" />
+    </div>
+</template>
+
+<script>
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+export default {
+    components: {
+        FooterGuide
+    }
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import './common/stylus/mixins.styl'
+.app
+    width 100%
+    height 100%
+    background #f5f5f5
+    a
+    text-decoration:none
+</style>
